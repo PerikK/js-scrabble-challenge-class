8c7cbfa852a5487a66e2a69b8b43739fb1a789f9
Periklis <perik76@gmail.com>
2024-05-29 00:32:53 +0300

    Mostly done - null returns 0 not working

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,73 @@
 class Scrabble {
   // Write your implementation here
+  constructor(word) {
+    this.points = 0
+    this.word = word.toUpperCase()
+    this.onePoint = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+    this.twoPoints = ['D', 'G']
+    this.threePoints = ['B', 'C', 'M', 'P']
+    this.fourPoints = ['F', 'H', 'V', 'W', 'Y']
+    this.fivePoints = ['K']
+    this.eightPoints = ['J', 'X']
+    this.tenPoints = ['Q', 'Z']
+  }
 
+  isValid(word) {
+    const allLetters = [
+      ...this.onePoint,
+      ...this.twoPoints,
+      ...this.threePoints,
+      ...this.fourPoints,
+      ...this.fivePoints,
+      ...this.eightPoints,
+      ...this.tenPoints
+    ]
+
+    if (word === null) {
+      this.points = 0
+    }
+
+    for (let i = 0; i < word.length; i++) {
+      if (word.length > 0 && allLetters.includes(word[i])) {
+        return true
+      }
+    }
+  }
+
+  score() {
+    if (this.isValid(this.word)) {
+      for (const i of this.word) {
+        if (this.onePoint.includes(i)) {
+          this.points += 1
+        }
+        if (this.twoPoints.includes(i)) {
+          this.points += 2
+        }
+        if (this.threePoints.includes(i)) {
+          this.points += 3
+        }
+        if (this.fourPoints.includes(i)) {
+          this.points += 4
+        }
+        if (this.fivePoints.includes(i)) {
+          this.points += 5
+        }
+        if (this.eightPoints.includes(i)) {
+          this.points += 8
+        }
+        if (this.tenPoints.includes(i)) {
+          this.points += 10
+        }
+      }
+    }
+    return this.points
+  }
 }
+
+// tst = new Scrabble('')
+// tst.isValid('k')
+let game = new Scrabble('hello')
+console.log(game.word);
+console.log(game.score())
 
 module.exports = Scrabble
